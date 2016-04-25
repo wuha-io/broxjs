47d0d670887d3da21840415dc1a0da6ad53ebedd
Brice Colucci <bcolucci@wuha.io>
2016-04-25 13:26:05 +0200

    wip

--- build/bin/broxjs.js
+++ build/bin/broxjs.js
@@ -2,10 +2,86 @@
 
 'use strict';
 
-var _yesno = require('yesno');
+var _bluebird = require('bluebird');
 
-var _yesno2 = _interopRequireDefault(_yesno);
+var _bluebird2 = _interopRequireDefault(_bluebird);
+
+var _async = require('async');
+
+var _async2 = _interopRequireDefault(_async);
+
+var _gitConfig = require('git-config');
+
+var _gitConfig2 = _interopRequireDefault(_gitConfig);
+
+var _prompt = require('prompt');
+
+var _prompt2 = _interopRequireDefault(_prompt);
+
+var _safe = require('colors/safe');
+
+var _safe2 = _interopRequireDefault(_safe);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_yesno2.default.ask('Are you sure you want to continue?', true, console.log.bind(console));
+var fs = _bluebird2.default.promisifyAll(require('fs-extra'));
+
+var CUR_DIR = process.cwd();
+var TPL_DIR = '/usr/local/lib/node_modules/broxjs/template';
+
+var error = function error(err) {
+  console.error(err);
+  process.exit(1);
+};
+
+(0, _gitConfig2.default)(function (err, gconfig) {
+
+  var user = gconfig.user.name || 'John Doe';
+  var email = gconfig.user.email || 'johndoe@iap.com';
+
+  var schema = {
+    properties: {
+      name: {
+        message: 'Extension\'s name',
+        default: 'My Extension',
+        required: true
+      },
+      version: {
+        message: 'Starting version',
+        default: '0.1.0',
+        required: true
+      },
+      author: {
+        message: 'Author',
+        default: user + ' <' + email + '>'
+      },
+      description: {
+        message: 'Description',
+        default: 'This is a default description.'
+      },
+      homepageURL: {
+        message: 'Homepage URL'
+      }
+    }
+  };
+
+  _prompt2.default.message = _safe2.default.red('Broxjs');
+  _prompt2.default.start();
+
+  _prompt2.default.get(schema, function (err, result) {
+
+    if (err) return error(err);
+
+    var extInfos = require(TPL_DIR + '/extension.json');
+    extInfos.name = extInfos.browser_action.default_title = result.name;
+    extInfos.version = result.version;
+    extInfos.author = result.author;
+    extInfos.description = result.description;
+    extInfos.extension_url = result.homepageURL;
+    extInfos.update_url = result.homepageURL + '/update';
+
+    //console.log(JSON.stringify(extInfos, null, 2));
+
+    fs.writeJson(CUR_DIR + '/extension.json', extInfos, { spaces: 2 });
+  });
+});
--- src/bin/broxjs.js
+++ src/bin/broxjs.js
@@ -1,6 +1,71 @@
 #!/usr/bin/env node
 'use strict';
 
-import yesno from 'yesno';
+import Promise from 'bluebird';
+import async from 'async';
+import gitconfig from 'git-config';
+import prompt from 'prompt';
+import colors from 'colors/safe';
 
-yesno.ask('Are you sure you want to continue?', true, console.log.bind(console));
+const fs = Promise.promisifyAll(require('fs-extra'));
+
+const CUR_DIR = process.cwd();
+const TPL_DIR = '/usr/local/lib/node_modules/broxjs/template';
+
+const error = err => {
+  console.error(err);
+  process.exit(1);
+};
+
+gitconfig((err, gconfig) => {
+
+  const user = gconfig.user.name || 'John Doe';
+  const email = gconfig.user.email || 'johndoe@iap.com';
+
+  var schema = {
+    properties: {
+      name: {
+        message: 'Extension\'s name',
+        default: 'My Extension',
+        required: true
+      },
+      version: {
+        message: 'Starting version',
+        default: '0.1.0',
+        required: true
+      },
+      author: {
+        message: 'Author',
+        default: user + ' <' + email + '>'
+      },
+      description: {
+        message: 'Description',
+        default: 'This is a default description.'
+      },
+      homepageURL: {
+        message: 'Homepage URL'
+      }
+    }
+  };
+
+  prompt.message = colors.red('Broxjs');
+  prompt.start();
+
+  prompt.get(schema, (err, result) => {
+
+    if (err) return error(err);
+
+    const extInfos = require(TPL_DIR + '/extension.json');
+    extInfos.name = extInfos.browser_action.default_title = result.name;
+    extInfos.version = result.version;
+    extInfos.author = result.author;
+    extInfos.description = result.description;
+    extInfos.extension_url = result.homepageURL;
+    extInfos.update_url = result.homepageURL + '/update';
+    //console.log(JSON.stringify(extInfos, null, 2));
+
+    fs.writeJson(CUR_DIR + '/extension.json', extInfos, {spaces: 2});
+
+  });
+
+});
